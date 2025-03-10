6e22581c7608222bb589f328ef30afde02f90f7d
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2025-03-10 19:29:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.java
+++ Difficulty: Hard/Edit Distance/edit-distance.java
@@ -9,12 +9,13 @@ class GFG {
         int T = Integer.parseInt(br.readLine().trim());
         while (T-- > 0) {
             String s1 = br.readLine();
-            String[] S = s1.split(" ");
-            String s = S[0];
-            String t = S[1];
+            String s2 = br.readLine();
+
             Solution ob = new Solution();
-            int ans = ob.editDistance(s, t);
+            int ans = ob.editDistance(s1, s2);
             System.out.println(ans);
+
+            System.out.println("~");
         }
     }
 }
@@ -23,26 +24,29 @@ class GFG {
 
 
 class Solution {
-    public int editDistance(String str1, String str2) {
-        int n=str1.length();
-        int m=str2.length();
-        int dp[][]=new int[n+1][m+1];
-        
-        for(int j=0;j<=m;j++) dp[0][j]=j;
-        for(int i=0;i<=n;i++)dp[i][0]=i;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(str1.charAt(i-1)==str2.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1];
-                }else {
-                    int insert=1+dp[i][j-1];
-                    int remove=1+dp[i-1][j];
-                    int replace=1+dp[i-1][j-1];
-                    
-                    dp[i][j]=Math.min(insert, Math.min(remove,replace));
-                }
-            }   
+    int[][] dp;
+
+    int solve(String s1, String s2, int i, int j) {
+        if (i == s1.length()) return s2.length() - j;  // Insert remaining characters
+        if (j == s2.length()) return s1.length() - i;  // Delete remaining characters
+
+        if (dp[i][j] != -1) return dp[i][j];  // DP memoization check
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            return dp[i][j] = solve(s1, s2, i + 1, j + 1);  // No change needed
+        } else {
+            int insert = 1 + solve(s1, s2, i, j + 1);      // Insert
+            int delete = 1 + solve(s1, s2, i + 1, j);      // Delete
+            int replace = 1 + solve(s1, s2, i + 1, j + 1); // Correct Replace logic
+            return dp[i][j] = Math.min(insert, Math.min(delete, replace));
         }
-        return dp[n][m];
+    }
+
+    public int editDistance(String s1, String s2) {
+        dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1); 
+        }
+        return solve(s1, s2, 0, 0);
     }
 }
