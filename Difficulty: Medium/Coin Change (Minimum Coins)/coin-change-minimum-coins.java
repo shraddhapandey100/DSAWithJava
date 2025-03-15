a97d5246cda2bd14b18ecf870a4a7a0ba82e4faa
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2025-03-15 22:15:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Coin Change (Minimum Coins)/coin-change-minimum-coins.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+class Solution {
+
+    public int minCoins(int coins[], int sum) {
+        // code here
+        int[]dp = new int[sum +1];
+        Arrays.fill(dp, sum +1);
+        int n = coins.length;
+        dp[0] = 0;
+        for(int num: coins){
+            for(int j= num; j<=sum; j++){
+                dp[j] = Math.min(dp[j], 1+dp[j - num]);
+            }
+        }
+        return dp[sum] == sum+1?-1:dp[sum];
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.minCoins(arr, k);
+
+            System.out.println(res);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
