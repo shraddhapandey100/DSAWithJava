4b5c342a6bbf1ee8d774cca187c5164a5b214039
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2025-03-16 12:35:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.java
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -1,22 +1,33 @@
 //{ Driver Code Starts
+// Initial Template for Java
+
 import java.io.*;
-import java.lang.*;
 import java.util.*;
 
-class GFG {
-    public static void main(String[] args) throws IOException {
+public class Main {
 
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
+        int t;
+        t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
-            int size = Integer.parseInt(br.readLine());
-            String[] arrStr = ((String)br.readLine()).split("\\s+");
-            int[] arr = new int[size];
-            for (int i = 0; i < size; i++) {
-                arr[i] = Integer.parseInt(arrStr[i]);
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
             }
-            System.out.println(new Solution().minJumps(arr, size));
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            System.out.println(new Solution().minJumps(arr));
+            System.out.println("~");
         }
     }
 }
@@ -25,36 +36,24 @@ class GFG {
 
 
 class Solution {
-    static int minJumps(int[] arr, int n) {
-        if (n == 0 || n == 1) {
-            return 0;
+    static int minJumps(int[] arr) {
+        // code here
+        int n = arr.length;
+        // Array to memoize values.
+        int[]dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[n-1] =0;
+        for(int i= n-2; i>=0; i--){
+            for(int j = i+1; j<= i + arr[i] && j<n; j++){
+                if(dp[j] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i], 1 + dp[j]);
+                }
+            }
         }
-        if (arr[0] == 0) {
+        //If end cannot be reached.
+        if(dp[0] == Integer.MAX_VALUE){
             return -1;
         }
-
-        int maxReach = arr[0];
-        int step = arr[0];
-        int jump = 1;
-
-        for (int i = 1; i < n; i++) {
-            if (i == n - 1) {
-                return jump;
-            }
-
-            maxReach = Math.max(maxReach, i + arr[i]);
-            step--;
-
-            if (step == 0) {
-                jump++;
-
-                if (i >= maxReach) {
-                    return -1;
-                }
-                step = maxReach - i;
-            }
-        }
-
-        return -1;
+        return dp[0];
     }
 }
