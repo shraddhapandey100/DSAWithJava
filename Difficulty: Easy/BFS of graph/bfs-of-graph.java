f6b2f3bdd292a93928011eedfb2626125864c1c2
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2025-04-02 18:32:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,59 +1,68 @@
 //{ Driver Code Starts
 // Initial Template for Java
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+// User function Template for Java
+class Solution {
+    // Function to return Breadth First Search Traversal of given graph.
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        ArrayList<Integer> traversal = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        int size = adj.size();
+        boolean[]visited = new boolean[size];
+        queue.add(0);
+        while(!queue.isEmpty()){
+            int pop = queue.poll();
+            traversal.add(pop);
+            for(int i=0; i< adj.get(pop).size(); i++){
+                int current = adj.get(pop).get(i);
+                if(!traversal.contains(current) && visited[current] != true){
+                    queue.add(current);
+                    visited[current] = true;
+                }
+            }
+        }
+        return traversal;
+    }
+}
+
+
+//{ Driver Code Starts.
+
 class GFG {
     public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+            int V = Integer.parseInt(br.readLine().trim());
             ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
-            for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                // adj.get(v).add(u);
+
+            for (int i = 0; i < V; i++) {
+                String[] input = br.readLine().trim().split(" ");
+                ArrayList<Integer> node = new ArrayList<>();
+                for (String s : input) {
+                    if (!s.isEmpty()) {
+                        node.add(Integer.parseInt(s));
+                    }
+                }
+                adj.add(node);
             }
+
             Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
-            for (int i = 0; i < ans.size(); i++)
-                System.out.print(ans.get(i) + " ");
+            ArrayList<Integer> ans = obj.bfs(adj);
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
             System.out.println();
+            System.out.println("~");
         }
     }
 }
 
 // } Driver Code Ends
-
-
-class Solution {
-    // Function to return Breadth First Traversal of given graph.
-    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
-        // Code here
-        ArrayList<Integer> bfsResult = new ArrayList<>();
-        boolean[] visited = new boolean[V];
-        Queue<Integer> queue = new LinkedList<>();
-        //Start BFS from the first node (assuming node 0).
-        queue.add(0);
-        visited[0] = true;
-        while(!queue.isEmpty()){
-            int node = queue.poll();
-            bfsResult.add(node);
-            //Get all adjacent vertices of the dequeued node.
-            for(int neighbor : adj.get(node)){
-                if(!visited[neighbor]){
-                    queue.add(neighbor);
-                    visited[neighbor] = true;
-                }
-            }
-        }
-        return bfsResult;
-    }
-}
