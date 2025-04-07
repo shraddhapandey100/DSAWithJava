c28f78e5a371d1f591ea6ed4e561063a1763d921
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2025-04-07 18:05:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,64 +1,65 @@
 //{ Driver Code Starts
-import java.util.*;
 import java.io.*;
-import java.lang.*;
+import java.util.*;
 
-class DriverClass {
+class GFG {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
             int V = sc.nextInt();
             int E = sc.nextInt();
-            for (int i = 0; i < V; i++)
-                list.add(i, new ArrayList<Integer>());
+            int[][] edges = new int[E][2];
             for (int i = 0; i < E; i++) {
-                int u = sc.nextInt();
-                int v = sc.nextInt();
-                list.get(u).add(v);
+                edges[i][0] = sc.nextInt();
+                edges[i][1] = sc.nextInt();
             }
-            if (new Solution().isCyclic(V, list) == true)
-                System.out.println("1");
-            else
-                System.out.println("0");
+
+            Solution obj = new Solution();
+            boolean ans = obj.isCyclic(V, edges);
+            System.out.println(ans ? "true" : "false");
         }
+        sc.close();
     }
 }
 // } Driver Code Ends
 
 
-/*Complete the function below*/
-
 class Solution {
-    // Function to detect cycle in a directed graph.
-    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
-        int vis[] = new int[V];
-        int pathVis[] = new int[V];
-        for(int i = 0; i < V; i++) {
-            if(vis[i] == 0) {
-                if(dfsCheck(i, adj, vis, pathVis)) {
+    public boolean isCyclic(int V, int[][] edges) {
+        // Step 1: uild adjacency list.
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<V; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge: edges){
+             adj.get(edge[0]).add(edge[1]);
+        }
+        boolean[] visited = new boolean[V];
+        boolean[]recStack = new boolean[V];
+        //Step 3: DFS for each vertex.
+        for(int i=0; i<V; i++){
+            if(!visited[i]){
+                if(dfs(i, adj, visited, recStack)){
                     return true;
                 }
             }
         }
         return false;
     }
-
-    private static boolean dfsCheck(int node, ArrayList<ArrayList<Integer>> adj, int vis[], int pathVis[]) {
-        vis[node] = 1;
-        pathVis[node] = 1;
-        for(int it: adj.get(node)) {
-            if(vis[it] == 0) {
-                if(dfsCheck(it, adj, vis, pathVis)) {
+    public static boolean dfs(int node, List<List<Integer>> adj, boolean[] visited, boolean[] recStack){
+        visited[node] = true;
+        recStack[node] = true;
+        for(int neighbor: adj.get(node)){
+            if(!visited[neighbor]){
+                if(dfs(neighbor, adj, visited, recStack)){
                     return true;
                 }
-            } else if(pathVis[it] == 1) {
+            }else if(recStack[neighbor]){
                 return true;
             }
         }
-        pathVis[node] = 0;
+        recStack[node] = false;
         return false;
     }
 }
